7202dad9db2d84583e14de5044c99fd5adc74f9b
KeerthiKollapu <keerthikollapu2025@gmail.com>
2026-04-16 21:55:35 +0530

    Time: 0 ms (100%), Space: 42.7 MB (87.88%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // both null → same
+        if (p == null && q == null) return true;
+
+        // one null or values differ → not same
+        if (p == null || q == null || p.val != q.val) return false;
+
+        // recursively check left and right subtrees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
